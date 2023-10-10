57d491a196e3ac7a3448a20d7f077df25b6c5b10
ZohaibAli1 <63587812+ZohaibAli1@users.noreply.github.com>
2023-10-10 12:11:12 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,73 +1,180 @@
+#include <fstream>
+#include <vector>
 #include <iostream>
-#include <graphics.h>
-#include <math.h>
+#include <string>
+#include <stdlib.h> 
 
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
+using namespace std;
 
-    int xc = getmaxx() / 2;
-    int yc = getmaxy() / 2;
-    int a = 200;
-    int b = 100;
-
-    int x = 0, y = b;
-    int a_sqr = a * a;
-    int b_sqr = b * b;
-    int delta = 4 * b_sqr * ((x + 1) * (x + 1)) + a_sqr * ((2 * y - 1) * (2 * y - 1)) - 4 * a_sqr * b_sqr;
-
-    while (a_sqr * (2 * y - 1) > 2 * b_sqr * (x + 1))
-    {
-        putpixel(xc + x, yc + y, WHITE);
-        putpixel(xc - x, yc + y, WHITE);
-        putpixel(xc + x, yc - y, WHITE);
-        putpixel(xc - x, yc - y, WHITE);
-
-        if (delta < 0)
-        {
-            x++;
-            delta += 4 * b_sqr * (2 * x + 3);
-        }
-        else
-        {
-            x++;
-            y--;
-            delta += 4 * b_sqr * (2 * x + 3) - 8 * a_sqr * y;
-        }
-    }
-
-    delta = b_sqr * ((2 * x + 1) * (2 * x + 1)) + 4 * a_sqr * ((y + 1) * (y + 1)) - 4 * a_sqr * b_sqr;
-
-    while (y + 1 != 0)
-    {
-        putpixel(xc + x, yc + y, WHITE);
-        putpixel(xc - x, yc + y, WHITE);
-        putpixel(xc + x, yc - y, WHITE);
-        putpixel(xc - x, yc - y, WHITE);
-
-        if (delta < 0)
-        {
-            y--;
-            x++;
-            delta += 4 * b_sqr * (2 * x + 2) - 8 * a_sqr * y;
-        }
-        else
-        {
-            y--;
-            delta += 4 * b_sqr * (2 * x + 2);
-        }
-    }
-
-    setfillstyle(SOLID_FILL, RED);
-    fillellipse(xc, yc, a, b);
-
-    setcolor(WHITE);
-    settextstyle(TRIPLEX_FONT, HORIZ_DIR, 3);
-    outtextxy(xc - textwidth("BC200409798") / 2, yc - b - textheight("BC200409798") - 10, "BC200409798");
-
-    getch();
-    closegraph();
-    return 0;
+void admin_view() {
+    cout << "Welcome, admin!" << endl;
+    cout << "1. Add Car" << endl;
+    cout << "2. Delete Car" << endl;
 }
+
+void user_view() {
+    cout << "Welcome, user!" << endl;
+    cout << "1. Register " << endl;
+    cout << "2. Login Existing" << endl;
+}
+
+int main() {
+    string manufactured, color, engine,name;
+    int miles;
+    string username, password;
+user_type:
+    int user_type;
+    cout << "Are you an admin or a regular user?" << endl;
+    cout << "1. Admin" << endl;
+    cout << "2. User" << endl;
+    cout << "Enter option: ";
+    cin >> user_type;
+
+    if (user_type == 1) {
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password: ";
+        cin >> password;
+        if (username == "admin" && password == "admin") {
+            admin_view();
+        }
+        else {
+            cout << "Invalid credentials. Aborting." << endl;
+            goto user_type;
+        }
+    }
+    else if (user_type == 2) {
+        user_view();
+    }
+    else {
+        cout << "Invalid option. Aborting." << endl;
+        goto user_type;
+    }
+
+    int option;
+    cout << "Enter option: ";
+    cin >> option;
+
+    if (user_type == 1) {
+        switch (option) {
+        case 1:
+        {
+            ofstream file("car_details.txt", ios::app);
+            if (file.is_open()) {
+                bool done = false;
+                while (!done) {
+                    file << "Add Car Details" << endl << endl;
+                    cout << "Company Made Car: ";
+                    cin >> manufactured;
+                    file << "Company Made Car: " << manufactured << endl;
+                    cout << "Color: ";
+                    cin >> color;
+                    file << "Color: " << color << endl;
+                    cout << "Engine HP: ";
+                    cin >> engine;
+                    file << "Engine HP: " << engine << endl;
+                    cout << "Miles Per KM: ";
+                    cin >> miles;
+                    file << "Miles Per KM: " << miles << endl;
+                    cout << "Add another car? (y/n) ";
+                    char response;
+                    cin >> response;
+                    if (response == 'n') {
+                        done = true;
+                        file.close();
+                        goto user_type;
+                    }
+                }
+            }
+            else {
+                cout << "Error: could not open file." << endl;
+            }
+        }
+        break;
+        case 2:
+        {
+            vector<string> cars;
+            ifstream file("car_details.txt");
+            if (file.is_open()) {
+                string line;
+                while (getline(file, line)) {
+                    cars.push_back(line);
+                }
+                file.close();
+            }
+            else {
+                cout << "Error: could not open file." << endl;
+            }
+
+            cout << "List of Cars:" << endl;
+            for (int i = 0; i < cars.size(); i++) {
+                cout << i + 1 << ". " << cars[i] << endl;
+            }
+            cout << "Enter the number of the car to delete: " << endl;
+        }
+        }
+    }
+    if (user_view) {
+        switch (option) {
+        case 1:{
+        	cout<<"Entre Details Below"<<endl;
+            ofstream file("user_details.txt", ios::app);
+            if (file.is_open()){
+            	bool done = false;
+            	while(!done){
+	            cout<<"Entre Your Name."<<endl;
+            	cin>>name;
+            	file.close();
+            goto user_type;   
+            } 
+        }
+			else {
+				cout<<"Could Not Write"<<endl;
+			} 
+			break;
+		}
+        case 2:
+		{
+		cout << "Enter Your Name." << endl;
+	    cin >> name;
+	    ifstream read_file("user_details.txt");
+	    bool name_exists = false;
+	    string line;
+	    while (getline(read_file, line)) {
+	        if (line == name) {
+	        name_exists = true;
+	        ifstream file("car_details.txt");
+	    	if (file.is_open()) {
+	        string line;
+	        while (getline(file, line)) {
+	            cout << line << endl;
+	        }
+	        file.close();
+	    }
+    else {
+        cout << "Error: could not open file." << endl;
+    }
+	    break;
+	            break;
+	        }
+	    }
+	    read_file.close();
+	    if (!name_exists) {
+	        cout << "Sorry, your name was not found in the file." << endl;
+	        break;
+	    }
+	    
+		    }         
+				break;
+		}        
+	}
+	    return 0;
+}
+    
+
+
+
+
+
+
 
